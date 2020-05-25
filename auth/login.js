061897f9c366bfcc72d8373b710f85aa056f96e2
mamox017 <mamox017@umn.edu>
2020-05-24 22:10:29 -0500

    login script added

---
+++ auth/login.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('login');
+
+btn.addEventListener('click', (event) => {
+	event.preventDefault();
+	console.log("logging in");
+	window.open("../dash/index.html", "_self");
+	//Search bar algorithm
+})
+
+function searchJobs() {
+
+}
