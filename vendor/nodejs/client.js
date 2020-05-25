55d2df5209903432ea569eb69b72871f34795290
mamox017 <mamox017@umn.edu>
2020-05-24 21:10:01 -0500

    initial commit

---
+++ vendor/nodejs/client.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('search');
+
+btn.addEventListener('click', (event) => {
+	event.preventDefault();
+	const job = document.getElementById('query').value;
+	console.log(job);
+	//Search bar algorithm
+})
+
+function searchJobs() {
+
+}
