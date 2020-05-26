1a587637a318f1d712fca0b3e0ace51a3d95ed27
Mootii Mamo <mamox017@umn.edu>
2020-05-26 15:00:39 -0500

    update button

--- vendor/nodejs/client.js
+++ vendor/nodejs/client.js
@@ -2,9 +2,10 @@ const btn = document.getElementById('search');
 
 btn.addEventListener('click', (event) => {
 	event.preventDefault();
-	const job = document.getElementById('query').value;
-	console.log(job);
+	//const job = document.getElementById('query').value;
+	//console.log(job);
 	//Search bar algorithm
+	window.location.href = "listings/listing.html"
 })
 
 function searchJobs() {
